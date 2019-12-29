f4251e19ea374343e0ae0577f776dc1a6e0647b4
Layla Habahbeh <laylahabahbeh@Laylas-MBP.attlocal.net>
2019-12-28 19:40:14 -0800

    Add jobs model and migrations

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobs.apps.JobsConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Where all media files will be stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Where someone goes to access the media
+MEDIA_URL = '/media/'
