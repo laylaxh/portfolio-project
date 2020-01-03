032bfce8f963ca0a0014b06f74c1ce1aa2e0ae48
Layla Habahbeh <laylahabahbeh@Laylas-MBP.attlocal.net>
2020-01-02 20:11:06 -0800

    Pulled job details from the db to display

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def home(request):
-	return render(request, 'jobs/home.html')
+	# Get all the jobs and send them back to the render
+	jobs = Job.objects
+	return render(request, 'jobs/home.html', {'jobs':jobs})
